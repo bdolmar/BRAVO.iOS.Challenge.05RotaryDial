40007d4e2612641c5bce3bc70a3dfa1a381dcf2d
Ben Dolmar <bdolmar@nerdery.com>
2014-02-28 12:42:06 -0600

    Initial Commit

---
+++ RotaryDial/NRDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NRDAppDelegate.h
+//  RotaryDial
+//
+//  Created by Ben Dolmar on 2/28/14.
+//  Copyright (c) 2014 Ben Dolmar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NRDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RotaryDial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RotaryDial
+//
+//  Created by Ben Dolmar on 2/28/14.
+//  Copyright (c) 2014 Ben Dolmar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NRDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NRDAppDelegate class]));
+    }
+}
